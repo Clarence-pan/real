b3c49884d4af5bc65672c12f8b1453d2fce07ed2
panchangyun <panchangyun@tuniu.com>
2014-09-12 16:37:37 +0800

    add some debug configurations (db, dump, bb_host)

--- index.php
+++ index.php
@@ -9,4 +9,5 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 require_once($yii);
+require_once('/../test/plib/dump/dump.php');
 Yii::createWebApplication($config)->run();
--- protected/config/beijing_database_config.inc.php
+++ protected/config/beijing_database_config.inc.php
@@ -7,28 +7,42 @@
  * Time: 04:03 PM
  * Description: database_config.inc.php
  */
-$databaseConfig = array(
-    'hagrid_master' => array(
-        'connectionString' => 'mysql:host=10.10.30.37;dbname=hagrid;port=3306',
-        'emulatePrepare' => true,
-        'username' => 'buckbeekdev',
-        'password' => 'tuniu520',
+if ("debuging"){
+    $db = array(
+        'connectionString' => 'mysql:host=127.0.0.1;dbname=buckbeek_mirror_beijing;port=3306',
+        'username' => 'root',
+        'password' => '',
         'class' => 'CDbConnection',
         'charset' => 'utf8',
         'enableProfiling' => YII_DEBUG,
         'enableParamLogging' => YII_DEBUG,
-    ),
-    'hagrid_slave' => array(
-        'connectionString' => 'mysql:host=10.10.30.37;dbname=hagrid;port=3306',
-        'emulatePrepare' => true,
-        'username' => 'buckbeekdev',
-        'password' => 'tuniu520',
-        'class' => 'CDbConnection',
-        'charset' => 'utf8',
-        'enableProfiling' => YII_DEBUG,
-        'enableParamLogging' => YII_DEBUG,
-    ),
-);
+        'autoConnect' => false
+    );
+    $databaseConfig = array('hagrid_master' => $db, 'hagrid_slave' => $db);
+}else{
+    $databaseConfig = array(
+        'hagrid_master' => array(
+            'connectionString' => 'mysql:host=10.10.30.37;dbname=hagrid;port=3306',
+            'emulatePrepare' => true,
+            'username' => 'buckbeekdev',
+            'password' => 'tuniu520',
+            'class' => 'CDbConnection',
+            'charset' => 'utf8',
+            'enableProfiling' => YII_DEBUG,
+            'enableParamLogging' => YII_DEBUG,
+        ),
+        'hagrid_slave' => array(
+            'connectionString' => 'mysql:host=10.10.30.37;dbname=hagrid;port=3306',
+            'emulatePrepare' => true,
+            'username' => 'buckbeekdev',
+            'password' => 'tuniu520',
+            'class' => 'CDbConnection',
+            'charset' => 'utf8',
+            'enableProfiling' => YII_DEBUG,
+            'enableParamLogging' => YII_DEBUG,
+        ),
+    );
+}
 
 $cacheConfig = array(
     array(
